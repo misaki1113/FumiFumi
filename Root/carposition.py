4032329319f6eaa2ae474c93284544d4e704626c
haruki4230 <ishimatsupanfumichimo22@gmail.com>
2024-12-16 15:26:04 +0900

    Update carposition.py

--- Root/carposition.py
+++ Root/carposition.py
@@ -2,38 +2,32 @@ import RPi.GPIO as GPIO
 import time
 
 class CarPosition:
-    # 定数
+    
     distance_move = 85  # 可動機の移動ステップ数
 
     def __init__(self):
-        # ボタンGPIO番号
-        self.leftbtn_gpio = 1   # 左操作盤
-        self.midbtn_gpio = 2    # 中央操作盤
-        self.rightbtn_gpio = 3  # 右操作盤
+        self.leftbtn_gpio = 6   # 左操作盤
+        self.midbtn_gpio = 19    # 中央操作盤
+        self.rightbtn_gpio = 26  # 右操作盤
 
-        # ステッピングモータ制御GPIO番号
-        self.steppingStep_gpio = 4  # STEP端子
-        self.steppingDir_gpio = 5   # DIR端子
-        self.steppingMs2_gpio = 6   # MS2端子
+        self.steppingStep_gpio = 20  # STEP端子
+        self.steppingDir_gpio = 21   # DIR端子
+        self.steppingMs2_gpio = 16   # MS2端子
 
-        # GPIO設定
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
 
-        # ボタンGPIO設定
         GPIO.setup(self.leftbtn_gpio, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.setup(self.midbtn_gpio, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.setup(self.rightbtn_gpio, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-        # ステッピングモータGPIO設定
         GPIO.setup(self.steppingStep_gpio, GPIO.OUT)
         GPIO.setup(self.steppingDir_gpio, GPIO.OUT)
         GPIO.setup(self.steppingMs2_gpio, GPIO.OUT)
 
-        # MS2端子を常にHIGHに設定
+        # 1/4マイクロステップモードをON
         GPIO.output(self.steppingMs2_gpio, GPIO.HIGH)
 
-        # 初期化
         self.isLeft = False
         self.isMid = False
         self.isRight = False
@@ -76,7 +70,7 @@ class CarPosition:
     def changePosition(self, step):
         if -10 < self.num_move < 10:
             # 移動するステップ数をdistance_moveに基づき設定
-            steps_to_move = self.distance_move  # 固定のステップ数
+            steps_to_move = self.distance_move
             # 方向を決定
             GPIO.output(self.steppingDir_gpio, GPIO.HIGH if step > 0 else GPIO.LOW)
 
@@ -91,25 +85,20 @@ class CarPosition:
             self.num_move += step
 
     def resetPosition(self):
-        # 位置リセット時の移動量計算
         steps_to_reset = self.num_move * self.distance_move
 
         # リセットが不要な場合に処理を終了
         if steps_to_reset == 0:
             return
         
-        # 方向を決定
+        # 方向を決定(正の値は前進、負の値は後退)
         GPIO.output(self.steppingDir_gpio, GPIO.LOW if steps_to_reset > 0 else GPIO.HIGH)
 
         # モータをリセット位置まで移動
         for _ in range(abs(steps_to_reset)):
             GPIO.output(self.steppingStep_gpio, GPIO.HIGH)
-            time.sleep(0.001)  # STEP信号の間隔
+            time.sleep(0.001)
             GPIO.output(self.steppingStep_gpio, GPIO.LOW)
             time.sleep(0.001)
 
-        # num_moveをリセット
         self.num_move = 0
-
-    def cleanup(self):
-        GPIO.cleanup()
