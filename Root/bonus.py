3eaa6965735471f8658348049f9e8b7412ad5cff
ihara <misabb2003@gmail.com>
2024-12-25 13:40:24 +0900

    add bonus.py

---
+++ Root/bonus.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import random
+
+result_bonus = 0
+
+class Bonus:
+
+    def __init__(self):
+        self.result_bonus = 0
+
+    def setBonus(self):
+        value_bonus = [10, 50, 100]
+        self.result_bonus = random.sample(value_bonus, 3)
+
+    def resultBonus(self, bonus_color):
+        color = bonus_color
+
+        if(color == "red"):
+            bonus = self.result_bonus[0]
+        elif(color == "blue"):
+            bonus = self.result_bonus[1]
+        else:
+            bonus== self.result_bonus[2]
+
+        return bonus
